ad4b08c8542410692afae3f6c05314b7f947ecc1
tommy-aki <141044473+tommy-aki@users.noreply.github.com>
2025-11-19 17:16:05 -0600

    visualizar todos los usuarios
    get http://localhost:3000/api/users/list
    
    404
    {
                    status: "Empty Response",
                    message: "No users found"
                }
    
    200
    {
                    status: "Success",
                    data: [{},...{}]
                }
    
    500
    {
                    status: "Error",
                    message: error.message
                }

--- campusConect-apt/routes/users.js
+++ campusConect-apt/routes/users.js
@@ -3,5 +3,6 @@ var router = require("express").Router()
 const userCtrl = require("../controllers/users")
 
 router.post('/new', userCtrl.createNewUser)
+router.get('/list', userCtrl.getAllUsers)
 
 module.exports = router;
